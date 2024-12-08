d0d8b8ccb69dc6c1d2f9d278bd97409014a301ce
Sahedul Islam Rony <sahedul.dev@gmail.com>
2024-12-08 18:33:45 +0600

    initial commit

---
+++ src/scripts/commit.mjs
@@ -0,0 +1,50 @@
+import chalk from "chalk";
+import { execSync } from "child_process";
+import readline from "readline";
+
+const run = (command) => {
+  try {
+    execSync(command, { stdio: "inherit" });
+  } catch (error) {
+    console.error(chalk.red(`Error: ${error.message}`));
+    process.exit(1);
+  }
+};
+
+const ask = (query) => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  return new Promise((resolve) => {
+    const coloredQuery = `${chalk.green(query)} [${chalk.green("y")}/${chalk.red("n")}]: `;
+    rl.question(coloredQuery, (answer) => {
+      rl.close();
+
+      const normalizedAnswer = answer.trim().toLowerCase();
+      if (["y", "yes"].includes(normalizedAnswer)) {
+        resolve(true);
+      } else {
+        resolve(false);
+      }
+    });
+  });
+};
+
+const main = async () => {
+  run("git add . && git commit");
+  const shouldPush = await ask(
+    "Do you want to push to your main branch (remote)?"
+  );
+
+  // Act based on user input
+  if (shouldPush) {
+    run("git push -u origin main");
+    process.stdout.write(chalk.green("Changes were pushed successfully.\n"));
+  } else {
+    console.log(chalk.yellow("Aborted. No changes were pushed."));
+  }
+};
+
+main();
